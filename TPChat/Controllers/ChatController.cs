20c3cf65edfeafd29866161022ec9f114f9bd882
Sakagaouya <sara.denoual2018@campus-eni.fr>
2020-03-20 10:05:34 +0100

    TPChat

---
+++ TPChat/Controllers/ChatController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TPChat.Models;
+using TPChat.Utils;
+
+namespace TPChat.Controllers
+{
+    public class ChatController : Controller
+    {
+        // GET: Chat
+        public ActionResult Index()
+        {
+            return View(FakeDbCat.Instance.Chats);
+        }
+
+        // GET: Chat/Details/5
+        public ActionResult Details(int id)
+        {
+            //ou utiliser requete SQL
+            Chat chat = FakeDbCat.Instance.Chats.FirstOrDefault(c => c.Id == id);
+            if (chat != null)
+            {
+                return View(chat);
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        // GET: Chat/Delete/5
+        public ActionResult Delete(int id)
+        {
+            //ou utiliser requete SQL
+            Chat chat = FakeDbCat.Instance.Chats.FirstOrDefault(c => c.Id == id);
+            if (chat != null)
+            {
+                return View(chat);
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        // POST: Chat/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                Chat chat = FakeDbCat.Instance.Chats.FirstOrDefault(c => c.Id == id);
+                FakeDbCat.Instance.Chats.Remove(chat);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ TPChat/Models/Chat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TPChat.Models
+{
+    public class Chat
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public int Age { get; set; }
+        public string Couleur { get; set; }
+    }
+}
